6ee68e1c537831db235189627f733c7d4802abe0
Eiji Tomura <s1f101803758@iniad.org>
2019-04-13 21:51:37 +0900

    dialog 周辺の js とタグを軽く生成
    次から、バックエンド側の処理を深めて完了させる

---
+++ bok/static/sub.js
@@ -0,0 +1,29 @@
+﻿function count_n(){
+    var t_num = document.getElementById('num');
+    t_num.textContent++;
+    var num = t_num.textContent;
+    var hid = document.getElementById("hid");
+    hid.value = num ;
+}
+
+
+
+function log_t(){
+    var text=document.getElementById('run')
+    var title=document.getElementById('d_title')
+    if (text.textContent == 'START'){
+        text.textContent = 'STOP';
+        title.textContent = 'Stop Timer';
+        timer=setInterval(count_n,1000);
+    }else{
+        text.textContent = 'START';
+        title.textContent = 'Start Timer';
+        clearInterval(timer)
+        d()
+    }
+}
+
+function rest_t(){
+    var num = document.getElementById('num');
+    num.textContent = 0;
+}
